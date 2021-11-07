fd8c8454d5105f1bca88cd5d8700a9de1bf2703a
Evren Kasap <evrkll9@gmail.com>
2021-11-07 09:49:51 +0300

    Add project files.

---
+++ fordongusu/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace fordongusu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("For örneği");
+
+            Console.WriteLine("Kaç adet sayı girdiniz. ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int toplam = 0;
+            
+            for (int i = 0 ; i < n; i++)
+            {
+                Console.WriteLine($"{i + 1 }.Sayıyı girdiniz.");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                              
+                toplam += sayi;
+
+            }
+
+            Console.WriteLine($"Girilen sayıların toplamı: {toplam} 'dır.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ whileDongusu/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace whileDongusu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 0;
+            do
+            {
+                Console.WriteLine($"Hello DoWhile! {sayac++}");
+            } while (sayac < 10);
+            
+            while(false)
+            {
+                Console.WriteLine("Hello While!");
+            }
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
